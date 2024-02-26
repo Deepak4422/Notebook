90c4364443370c7edf55a4adcca181d076c38c82
Deepak4422 <deepakkumarka442@gmail.com>
2024-02-26 23:51:52 +0530

    changes to baackend index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const app=express();
 var cors = require('cors')
 
 app.use(cors())
+app.use(cors({
+    origin: ['https://deploy-mern-1whq.vercel.app'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
